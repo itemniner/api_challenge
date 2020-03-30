20494e4b1b300b4c96afd1876ac61c51f7d3e1ea
Raymond Nguyen <rayknguyen@gmail.com>
2020-03-30 10:27:40 -0600

    Add

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,9 @@ require 'faraday'
 
 class SearchesController 
   def index
-    conn = Faraday.post(url: "https://api.nal.usda.gov/fdc/v1/search?api_key=hfBNMzxwRcKN0u7SzYR4b30m34skjPaOehvPaLAa") do |f|
+    conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/search?") do |f|
+    f.headers["api_key"] = 'hfBNMzxwRcKN0u7SzYR4b30m34skjPaOehvPaLAa'
+    f.headers["generalSearchInput"] = "#{search_inquiry}"
       
     end
   end
